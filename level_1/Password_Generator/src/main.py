bb219b5c068837d099fc015d27da3db74837d0c9
amir abbas <warzone8419@gmail.com>
2025-08-16 11:29:16 +0330

    add README.md file upper words based on the upper probability for memorable password

--- level_1/Password_Generator/src/main.py
+++ level_1/Password_Generator/src/main.py
@@ -11,20 +11,37 @@ nltk.download('words')
 
 
 class PasswordGenerator(ABC):
+    """Abstract base class for password generators.
+
+    Args:
+        ABC (type): Abstract base class.
+    """
     @abstractmethod
-    def generate(self):
+    def generate(self) -> None:
+        """Generate a password.
+        """
         pass
 
 
 class PinPasswordGenerator(PasswordGenerator):
+    """PIN password generator.
+
+    Args:
+        PasswordGenerator (ABC): Abstract base class for password generators.
+    """
     def __init__(self, length: int = 8):
         self.length = length
-    
-    def generate(self):
+
+    def generate(self) -> str:
         return ''.join([random.choice(string.digits) for _ in range(self.length)])
     
 
 class RandomPasswordGenerator(PasswordGenerator):
+    """Random password generator.
+
+    Args:
+        PasswordGenerator (ABC): Abstract base class for password generators.
+    """
     def __init__(self, length: int = 8, include_digits: bool = True, include_symbols: bool = True):
         self.length = length
         self.characters = string.ascii_letters
@@ -32,28 +49,39 @@ class RandomPasswordGenerator(PasswordGenerator):
             self.characters += string.digits
         if include_symbols:
             self.characters += string.punctuation
-        
-    def generate(self):
+
+    def generate(self) -> str:
         return ''.join([random.choice(self.characters) for _ in range(self.length)])
     
 
 class MemorablePasswordGenerator(PasswordGenerator):
-    def __init__(self, length: int = 4, word_list: list = None):
+    """Memorable password generator.
+
+    Args:
+        PasswordGenerator (ABC): Abstract base class for password generators.
+    """
+    def __init__(self, length: int = 4, word_list: list = None, upper: float = 0.5):
         if word_list is None:
             word_list = nltk.corpus.words.words()
         self.length = length
         self.word_list = word_list
+        self.upper = upper
 
-    def generate(self):
-        return '-'.join([random.choice(self.word_list) for _ in range(self.length)]) 
-    
+    def generate(self) -> str:
+        word = [random.choice(self.word_list) for _ in range(self.length)] # Generate a list of random words
+        # upper words based on the upper probability
+        for i, w in enumerate(word):
+            if random.random() < self.upper:
+                word[i] = w.upper()
+        return '-'.join(word)
+
 
 if __name__ == "__main__":
-    pin_generator = PinPasswordGenerator(length=6)
+    pin_generator = PinPasswordGenerator(length=8)
     print("PIN Password:", pin_generator.generate())
 
     random_generator = RandomPasswordGenerator(length=12)
     print("Random Password:", random_generator.generate())
 
-    memorable_generator = MemorablePasswordGenerator(length=3)
+    memorable_generator = MemorablePasswordGenerator(length=4)
     print("Memorable Password:", memorable_generator.generate())
