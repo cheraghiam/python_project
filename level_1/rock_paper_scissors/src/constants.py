1018dcc006e739661cfdebc82c8c8523b2953570
amirabbas <warzone8419@gmail.com>
2025-09-20 16:24:01 +0330

    add __name__ == __main__ to check_result

--- level_1/rock_paper_scissors/src/constants.py
+++ level_1/rock_paper_scissors/src/constants.py
@@ -1,36 +1,7 @@
 game_items = ['rock', 'paper', 'scissors']
+
 winner = [
     ('rock', 'scissors'),
     ('scissors', 'paper'),
     ('paper', 'rock')
 ]
-
-
-class RockPaperScissors:
-    def check_result(self, item_computer, item_user):
-        if item_computer == item_user:
-            return "Its a Tie"
-
-        for stage_win in winner:
-            if stage_win[0] == item_user and stage_win[1] == item_computer:
-                return "you win, Computer loses"
-        return "Computer win, you loses"
-
-    def play_game(self, user_choice):
-        item_computer = self.get_item_from_computer()
-        item_user = self.get_item_from_user(user_choice)
-        print(f"item computer is {item_computer}, your choice {item_user}")
-        print(self.check_result(item_computer, item_user))
-        again_play = input("Do you want to play again? (yes/no): ")
-        if again_play.lower() == "yes":
-            user_choice = input(f"Enter your choice ({['rock', 'paper', 'scissors']}): ")
-            self.play_game(user_choice=user_choice)
-        else:
-            print("Thank you for playing!")
-
-
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    user_choice = input(f"Enter your choice ({['rock', 'paper', 'scissors']}): ")
-    rps = RockPaperScissors(user_name)
-    rps.play_game(user_choice=user_choice)
