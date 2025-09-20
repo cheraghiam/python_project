814d3efeefd04de3c8cfd1153e825e8261a7623c
amirabbas <warzone8419@gmail.com>
2025-09-20 16:00:05 +0330

    add get_from_user.py module

---
+++ level_1/rock_paper_scissors/src/get_from_user.py
@@ -0,0 +1,13 @@
+from src.main import game_items
+
+
+def get_user_choice():
+    input_user = input("Enter your choice  ['rock', 'paper', 'scissors']: ")
+    if input_user.lower() in set(game_items):
+        return input_user
+    else:
+        get_user_choice()
+
+
+if __name__ == "__main__":
+    print(get_user_choice())
