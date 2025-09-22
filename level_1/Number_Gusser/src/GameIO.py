19468c0c2ff6d8fb9bb89ab2cb4aaf1f52aee6b3
amirabbas <warzone8419@gmail.com>
2025-09-22 23:29:38 +0330

    add Gameio.py file for input output

---
+++ level_1/Number_Gusser/src/GameIO.py
@@ -0,0 +1,20 @@
+import random
+
+
+target_number = random.randint(1, 100)
+
+
+def user_guess():
+    while True:
+        input_user = input("Enter Your Guess Number: ")
+        try:
+            return int(input_user)
+        except ValueError:
+            print("Your Guess Should be Number")
+
+
+if __name__ == "__main__":
+    print(target_number())
+    user_guess = user_guess()
+    print(user_guess)
+    
